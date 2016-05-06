5178ed6d74a0b8deafaafb4f3bcf2b3684294f60
iceberg20 <italoberg@yahoo.com.br>
2016-05-06 07:14:37 -0300

    Criando classes de dominio e adicionando notação do lombok  Fixes #8

---
+++ src/main/java/dominio/Usuario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author italo
+ */
+
+@Getter
+@Setter
+@Builder
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private int nivel;
+}
