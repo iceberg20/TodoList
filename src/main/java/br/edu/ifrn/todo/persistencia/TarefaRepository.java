7081f239e3e513fa9beda928c69fc0374b430f7f
LuanGB <luan.goncbs@gmail.com>
2016-06-09 19:39:54 -0300

    fix iceberg20/TodoList#35

--- src/main/java/br/edu/ifrn/todo/persistencia/TarefaRepository.java
+++ src/main/java/br/edu/ifrn/todo/persistencia/TarefaRepository.java
@@ -1,14 +1,9 @@
 package br.edu.ifrn.todo.persistencia;
 
 import br.edu.ifrn.todo.dominio.Tarefa;
-import java.util.Iterator;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
-public interface TarefaRepository {
-
-    void save(Tarefa objeto);
-
-    void delete(Tarefa objeto);
-    
-    Iterator<Tarefa> iterator();
+public interface TarefaRepository extends CrudRepository<Tarefa, Long>, QueryByExampleExecutor<ContaCredito> {
     
 }
