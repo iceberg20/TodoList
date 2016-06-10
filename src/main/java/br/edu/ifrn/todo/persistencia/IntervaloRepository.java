c76d08139f3f0fba117842c3fbbb15ba0e5b15be
Julia Martins <mjuh.ms@gmail.com>
2016-06-10 17:23:00 -0300

    correçoes minimas

--- src/main/java/br/edu/ifrn/todo/persistencia/IntervaloRepository.java
+++ src/main/java/br/edu/ifrn/todo/persistencia/IntervaloRepository.java
@@ -1,7 +1,6 @@
 package br.edu.ifrn.todo.persistencia;
 
 import br.edu.ifrn.todo.dominio.Intervalo;
-import java.util.Iterator;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IntervaloRepository extends CrudRepository <Intervalo, Long>{
